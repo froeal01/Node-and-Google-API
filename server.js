96ec38f4cda2fbc3a41db18760f4a416c88bf178
Alex Froelich <alexfroelich@Alexs-MacBook-Pro.local>
2014-02-04 13:21:16 -0500

    added html.index

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ app = connect()
   .use(connect.static('app'))
   .use('/js/lib/', connect.static('node_modules/requirejs/'))
   .use('/node_modules', connect.static('node_modules'))
-  .use('/test', connect.static('test/'))
-  .use('/test', connect.static('app'))
   ;
 
 http.createServer(app).listen(5050, function() {
