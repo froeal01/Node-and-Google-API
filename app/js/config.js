0bb7c63d53c2bf095ee7cdbf6bdbf31382f99ee2
Alex Froelich <alexfroelich@Alexs-MacBook-Pro.local>
2014-02-04 19:42:04 -0500

    added file

---
+++ app/js/config.js
@@ -0,0 +1,7 @@
+define([], function(){
+	var config = {};
+	config.apiKey = "AIzaSyBB35jT9xNiPClIOpf2Lo40kqI447b1Jz4";
+  config.scopes = 'https://www.googleapis.com/auth/tasks https://www.googleapis.com/auth/userinfo.profile';
+  config.clientID = '491488844457-q539oarktu2h72tam7gnovdp49tq838m.apps.googleusercontent.com';
+  return config;
+});
