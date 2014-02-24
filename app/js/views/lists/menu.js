79c7e05a9f00a92a4ac30f417dc52b3a33559e09
Alex Froelich <alexfroelich@Alexs-MacBook-Pro.local>
2014-02-24 16:13:48 -0500

    menuitem.js

--- app/js/views/lists/menu.js
+++ app/js/views/lists/menu.js
@@ -12,6 +12,16 @@ define(['views/lists/menuitem'], function(ListMenuItemView){
 			this.collection.on('add', this.render, this);
 		},
 		render: function(){
+			var $el = $(this.el),
+				self = this;
+
+			this.collection.each(function(list){
+				var item, sidebarItem;
+				item = new ListMenuItemView({ model: list});
+				$el.append(item.render().el);
+			});
+
+			return this;
 
 		}
 	});
